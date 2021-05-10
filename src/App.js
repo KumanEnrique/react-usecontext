12cf21fdec5914628050a8989dd5eadd3437cb2e
KumanEnrique <36672948+KumanEnrique@users.noreply.github.com>
2021-05-09 20:32:04 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import React from "react";
+import UsersList from './components/UserList'
+import Profile from "./components/Profile";
+
+import UserState from "./context/User/UserState";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <UserState>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md-7">
+            <UsersList />
+          </div>
+          <div className="col-md-5">
+            <Profile />
+          </div>
+        </div>
+      </div>
+    </UserState>
+  );
+}
+
+export default App;
